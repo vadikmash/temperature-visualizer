0db106735e3c90c29f01ea2e27c0c15a7f8eb1c6
vadikmash <vadikmash@gmail.com>
2019-08-18 20:10:08 +0300

    fix: made it work

--- renderer.js
+++ renderer.js
@@ -1,8 +1,8 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
-const Serialport = require('serialport')
-Serialport.list().then(ports => {
+const SerialPort = require('serialport')
+SerialPort.list().then(ports => {
   document.getElementById("port-list").innerHTML = `
   <h1>Detected Serial Ports</h1>
   <ul>
@@ -10,3 +10,20 @@ Serialport.list().then(ports => {
   </ul>
   `
 })
+
+
+const portName = 'COM5';
+
+const port = new SerialPort(portName, {
+  baudRate: 115200,
+});
+
+
+port.on('open', () => console.log('connection opened'));
+
+
+port.on('data', (data) => {
+  const string = [...data].reduce((accum, d) => accum + String.fromCharCode(d), '')
+  console.log(string)
+});
+
