9ebba386516307ddd983e97c38c08d996a382757
vadikmash <vadikmash@gmail.com>
2019-08-29 22:28:17 +0300

    feat: RGB visualization improvement

--- src/visualizers/index.ts
+++ src/visualizers/index.ts
@@ -18,17 +18,38 @@ export const blue = (t, bottom, top) => {
   return `rgb(0, 0, ${shade})`
 }
 
+
 export const RGB = (t, bottom, top) => {
-  const shade = (t - bottom < 0 ? 0 : t - bottom) / (top - bottom) * 255 * 3
+  let shade = (t - bottom ) / (top - bottom) * 255 * 3 
+  shade = shade > 255 * 3 ? 255 * 3 : shade
+
   let R = 0
   let G = 0
   let B = 0
-  if (shade <= 255)
-    B = shade
-  if (255 < shade && shade <= 255 * 2)
-    G = shade % 255   
-  if (shade > 255 * 3)
-    R = shade % 255
+
+  // blue
+  if (shade < 128) {
+    B = shade * 2
+  }
+  if (shade < 256 && shade >= 128) {
+    B = (256 - shade) * 2
+  }
+  // green
+  if (shade < 256 + 128 - 64 && shade >= 256 - 64) {
+    G = (shade - (256 - 64)) * 2
+  }
+  if (shade < 512 - 64 && shade >= 256 + 128 - 64) {
+    G = ((512 - 64) - shade) * 2
+  }
+  // red
+  if (shade < 512 + 256 && shade >= 512 - 128) {
+    R = (shade - (512 - 128)) * 0.75
+  }
+  // white
+  if (shade < 512 + 256 && shade >= 512) {
+    G = (shade - (512)) 
+    B = (shade - (512)) 
+  }
 
   return `rgb(${R}, ${G}, ${B})`
 }
